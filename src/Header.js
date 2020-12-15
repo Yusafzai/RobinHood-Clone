26cce0ed6045ed74350f26f66bc9567899f53626
Gefahr Yousafzai <itsyusafzai@gmail.com>
2020-12-15 20:40:53 +0500

    Added Top Component

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Logo from './Robinhood.svg';
+
+function Header() {
+    return (
+        <div className="header__wrapper">
+           
+            {/* logo */}
+            <div className="header__logo">
+                <img src={Logo} width={160} />
+            </div>
+
+           {/* menu */}
+           <div className="header__menuItems">
+              <a href="/">Products</a>
+              <a href="/">Learn</a>
+              <a href="/">Support</a>
+              <a href="/">Who we are</a>
+           </div>
+
+        {/* Login */}
+        <div className="header__log">
+             <a href="/">Log In</a>
+        </div>
+
+        {/* SignUp */}
+        <div className="header__sign">
+        <button class="header__button">Sign Up</button>
+        </div>
+
+        </div>
+    )
+}
+
+export default Header
